b5ad1f972c021f2bfbedb069110d8705c30d1fbf
SirSauce <66134796+SirSaucey@users.noreply.github.com>
2023-05-21 23:44:12 +0200

    Add files via upload

---
+++ site/script/responsive.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
